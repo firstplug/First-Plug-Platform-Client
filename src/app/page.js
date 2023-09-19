0a83fccf933939162b2d28b69869ee2f09ba93d3
Braian Barrientos <braianbts@gmail.com>
2023-09-19 16:41:31 -0300

    Install tailwind finished

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <main>
-      <h2>Hola Mundo!</h2>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </main>
   );
 }

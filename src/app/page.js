e05c6f4e34700818b91f2e0820f3c9ddb05637ed
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-20 15:13:21 -0300

    cleanning page.js file

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Alert from "../common/Alert";
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Alert msg="Your account has been successfully created." />
     </main>
   );
 }

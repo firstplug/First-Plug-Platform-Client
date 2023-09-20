465fefb4342f6369f2a73fb5ca4fa9114de98a64
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-20 11:01:07 -0300

    common button first version

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 
-export default function Button({ body, className, icon }) {
-  //CREAR UN IF O ALGO PARA LAS CLASES O ESTILOS
+export default function Button({ body, size, variant, icon }) {
+  const className = {
+    big: {
+      primary: "bg-blue text-white text-lg",
+      scondary: "bg-white  border-blue  text-blue text-lg",
+      text: "bg-white   text-blue text-lg",
+      alert: "text-error   text-lg",
+    },
+    small: {
+      primary: "bg-blue text-white text-md",
+      scondary: "bg-white  border-blue  text-blue text-md",
+      text: "bg-white   text-blue text-md",
+      alert: "text-error   text-md",
+    },
+  };
   return (
     <button
-      className={className}
+      className={className[size][variant]}
       style={{
-        padding: "9px 24px",
+        padding: size === "small" ? "9px 24px" : "12px 24px",
         borderRadius: "8px",
         width: "100%",
         textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        gap: "8px",
+        fontWeight: 700,
+        lineHeight: "22px",
       }}
     >
-      {icon}
+      <span>{icon}</span>
       {body}
     </button>
   );

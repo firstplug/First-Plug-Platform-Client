19e4714b9af54ccba10b64239d3f5a347efd04de
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-20 15:30:09 -0300

    firts commit nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </main>
   );
 }

13ee2a678ec7df02792cb8452db57840546ba637
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-09-20 09:57:13 -0300

    add custom className

--- src/common/CustomLink.jsx
+++ src/common/CustomLink.jsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
 
-export default function CustomLink({ href, text }) {
+export default function CustomLink({ href, text, className = "" }) {
   return (
-    <Link href={href} className="text-blue font-lg font-bold font-sans">
+    <Link
+      href={href}
+      className={`text-blue font-lg font-bold font-sans ${className}`}
+    >
       {text}
     </Link>
   );

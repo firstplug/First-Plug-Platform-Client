aa6aa01605c416b69924a1091ee057d918929022
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-21 17:05:15 -0300

    colaboratorCard work stop until sprint3

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,53 @@
+import ColaboratorCard from "@/components/ColaboratorCard";
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
+const array = [
+  {
+    name: "Francisco",
+    lastName: "Villanueva",
+    jobPosition: "Junior Dev",
+    products: ["mac", "phone"],
+    shimentsDetails: "incomplete",
+    team: "dev",
+  },
+  {
+    name: "Esteban",
+    lastName: "Rodriguez",
+    jobPosition: "Sernio Dev",
+    products: ["mac", "phone", "monitor"],
+    shimentsDetails: "complete",
+    team: "dev",
+  },
+
+  {
+    name: "Agustin",
+    lastName: "Sandoval",
+    jobPosition: "Sernio Dev",
+    products: ["mac", "phone"],
+    shimentsDetails: "complete",
+    team: "finance",
+  },
+
+  {
+    name: "Braian",
+    lastName: "Barrientos",
+    jobPosition: "Desing",
+    products: ["mac", "phone", "monitor"],
+    shimentsDetails: "incomplete",
+    team: "desing",
+  },
+];
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div className="grid grid-cols-1  gap-2 m-10">
+        {array.map((m) => (
+          <ColaboratorCard {...m} />
+        ))}
+      </div>
     </main>
   );
 }

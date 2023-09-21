ce1295a785ac37f933cea9709e8519b420031b7d
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-09-21 19:15:29 -0300

    create sidebarlink common and fix sidebar

--- src/common/Icons.jsx
+++ src/common/Icons.jsx
@@ -368,17 +368,15 @@ export function ComputerIcon() {
     </svg>
   );
 }
-export function DashboardIcon(className = "") {
+export function DashboardIcon(className) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      width={50}
-      height={50}
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6 "
+      className="w-6 h-6"
     >
       <path
         strokeLinecap="round"

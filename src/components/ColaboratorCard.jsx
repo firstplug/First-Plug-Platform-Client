4af969af792d989922c23e9773db2d06941689e0
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-22 15:14:25 -0300

    configured view of My Team

--- src/components/ColaboratorCard.jsx
+++ src/components/ColaboratorCard.jsx
@@ -3,44 +3,8 @@ import Photo from "../../public/Colaborator.png";
 import Image from "next/image";
 import Button from "@/common/Button";
 import TeamCard from "@/common/TeamCard";
+import { PenIcon, TrashIcon } from "@/common/Icons";
 
-function PenIcon({ className, stroke }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={stroke || 1.5}
-      stroke="currentColor"
-      className={`w-6 h-6 ${className}`}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-      />
-    </svg>
-  );
-}
-
-function TrashIcon({ className, stroke }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={stroke || 1.5}
-      stroke="currentColor"
-      className={`w-6 h-6 ${className}`}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-      />
-    </svg>
-  );
-}
 export default function ColaboratorCard({
   name,
   lastName,
@@ -49,9 +13,12 @@ export default function ColaboratorCard({
   products,
   shimentsDetails = "incomplete",
   team,
+  className,
 }) {
   return (
-    <article className="flex flex-col gap-2 w-[400px] mx-auto rounded-lg border border-border p-4 font-inter">
+    <article
+      className={`flex flex-col gap-2  mx-auto rounded-lg border border-border p-4 font-inter ${className}`}
+    >
       <header className="flex justify-between items-start">
         <div className="flex gap-2">
           <div>
@@ -71,20 +38,30 @@ export default function ColaboratorCard({
         </div>
         <div className="flex gap-1">
           <Button
-            icon={<PenIcon stroke={2} className="text-dark-grey  text-sm" />}
+            icon={
+              <PenIcon
+                stroke={2}
+                className="text-dark-grey w-[1.2rem] h-[1.2rem]"
+              />
+            }
           />
           <Button
-            body={<TrashIcon stroke={2} className=" text-dark-grey text-sm " />}
+            body={
+              <TrashIcon
+                stroke={2}
+                className=" text-dark-grey w-[1.2rem] h-[1.2rem]"
+              />
+            }
           />
         </div>
       </header>
       <section className="flex flex-col gap-2 justify-start">
-        <div className="flex   items-centergap-3">
-          <h2 className="font-semibold">Job Position:</h2>
+        <div className="flex   items-center gap-3">
+          <h2 className="font-semibold text-lg">Job Position: </h2>
           <p>{jobPosition}</p>
         </div>
         <div className="flex items-center  gap-3">
-          <h2 className="font-semibold">Products</h2>
+          <h2 className="font-semibold text-lg">Products</h2>
           <p className="bg-border  rounded-full h-6 w-6 text-center  grid place-items-center items text-sm">
             {products.length}
           </p>

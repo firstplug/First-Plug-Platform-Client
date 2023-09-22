013acac08b4bc7b70f366416be986a877fb29698
Braian Barrientos <braianbts@gmail.com>
2023-09-22 18:27:54 -0300

    common finished to add in table component and add icon in Icons.jsx

--- src/common/Icons.jsx
+++ src/common/Icons.jsx
@@ -498,3 +498,22 @@ export function UpLoadIcon({ className }) {
     </svg>
   );
 }
+
+export function ChevronDown({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class={`w-6 h-6 ${className}`}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}

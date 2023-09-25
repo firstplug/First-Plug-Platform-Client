578a7b673236cc533b86595d16e9cf2210e1dde8
Braian Barrientos <braianbts@gmail.com>
2023-09-25 17:36:58 -0300

    just modify the structure of tableRow and HorizontalSection, add a new icon too

--- src/common/TableRow.jsx
+++ src/common/TableRow.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { TrashIcon } from "@/common/Icons";
 import Button from "./Button";
+import HorizontalSection from "./HorizontalSection";
 
 const statusColors = {
   missingData: "bg-lightRed",
@@ -11,20 +12,35 @@ const statusColors = {
   shipped: "bg-lightBlue",
 };
 
-const TableRow = ({ id, name, status, actions }) => {
+const TableRow = ({ id, name, status, actions, className }) => {
   const statusColorClass = statusColors[status] || "";
   return (
-    <main className="w-full h-10 bg-white flex items-center justify-between p-8 gap-x-40 border border-grey">
-      <span className="text-black">{id}</span>
-      <span className="text-black">{name}</span>
-      <span
-        className={`text-black ${statusColorClass} px-[8px] py-[4px] rounded-[200px] font-montserrat text-[12px] font-semibold`}
-      >
-        {status.toUpperCase()}
-      </span>
-      <span className="text-black">{actions}</span>
-      <Button icon={<TrashIcon />} className="h-[32px] w-[32px]" />
-    </main>
+    <table
+      className={`w-full h-10 bg-white flex items-center justify-between p-8 gap-x-40 border-t-2 border-grey ${className} text-center `}
+    >
+      <tr>
+        <td className="text-black text-center">{id}</td>
+      </tr>
+      <tr>
+        <td className="text-black text-center">{name}</td>
+      </tr>
+      <tr>
+        <td
+          className={`text-black ${statusColorClass} px-[8px] py-[4px] rounded-[200px] font-montserrat text-[12px] font-semibold text-center`}
+        >
+          {status.toUpperCase()}
+        </td>
+      </tr>
+      <tr>
+        <td className="text-black font-semibold text-center">{actions}</td>
+      </tr>
+      <tr>
+        <Button
+          icon={<TrashIcon color="red" />}
+          className="h-[32px] w-[32px] color"
+        />
+      </tr>
+    </table>
   );
 };
 

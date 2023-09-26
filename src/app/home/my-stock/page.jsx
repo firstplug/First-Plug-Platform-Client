be01a2193082ca4987d32368c067ee33901cf86a
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-09-26 10:49:21 -0300

    aside loadstock finished

--- src/app/home/my-stock/page.jsx
+++ src/app/home/my-stock/page.jsx
@@ -10,6 +10,7 @@ import Aside from "@/components/Aside";
 import useModal from "@/hooks/useModal";
 import AddStockCard from "@/common/AddStockCard";
 import CustomLink from "@/common/CustomLink";
+import Image from "next/image";
 
 export default function MyStock() {
   const { isModalOpen, openModal, closeModal } = useModal();
@@ -45,9 +46,30 @@ export default function MyStock() {
       {isModalOpen && (
         <Aside title="Load Stock" closeModal={closeModal}>
           <div className="flex flex-col gap-6">
-            <Card className="border-dashed">dasdsas</Card>
-            <AddStockCard title="Stock_2023.cvs" file="443kb" />
-            <AddStockCard title="Stock_2023.cvs" file="443kb" />
+            <Card className="border-dashed border h-60 flex flex-col gap-4 justify-center">
+              <Image
+                alt="folder icon"
+                src="/svg/folder.svg"
+                width={84}
+                height={88}
+                className="mt-[-20px]"
+              />
+
+              <p>Drag and drop your CSV file here or</p>
+              <CustomLink href="#">
+                <h2>Select a File</h2>
+              </CustomLink>
+            </Card>
+            <AddStockCard
+              title="Stock_2023.cvs"
+              file="443kb"
+              currentDate="04/04/2023 12:41"
+            />
+            <AddStockCard
+              title="Stock_2023.cvs"
+              file="443kb"
+              currentDate="04/04/2023 12:41"
+            />
           </div>
 
           <div className="fixed bottom-5 w-[85%]">

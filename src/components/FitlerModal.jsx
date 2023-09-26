657ff58d802bff91a8aa28bcbce8f1e834109552
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-26 08:43:18 -0300

    adding select All function to filter modal

--- src/components/FitlerModal.jsx
+++ src/components/FitlerModal.jsx
@@ -1,7 +1,27 @@
+"use client";
 import SearchInput from "@/common/SearchInput";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function FitlerModal({ array, className }) {
+  const [allChecked, setAllChecked] = useState(false);
+  const [checkboxes, setCheckboxes] = useState([]);
+
+  useEffect(() => {
+    // Initialize the state for checkboxes based on the array length
+    setCheckboxes(Array(array.length).fill(false));
+  }, [array]);
+
+  const handleSelectAll = () => {
+    setAllChecked(!allChecked);
+    setCheckboxes(Array(array.length).fill(!allChecked));
+  };
+
+  const handleCheckboxChange = (index) => {
+    const newCheckboxes = [...checkboxes];
+    newCheckboxes[index] = !newCheckboxes[index];
+    setCheckboxes(newCheckboxes);
+    setAllChecked(newCheckboxes.every((isChecked) => isChecked));
+  };
   return (
     <div
       className={` ${
@@ -11,12 +31,21 @@ export default function FitlerModal({ array, className }) {
       <SearchInput />
       <div className="p-0 max-h-[7rem] overflow-y-auto flex flex-col gap-2">
         <div className="flex items-center gap-2 my-1">
-          <input type="checkbox" className="w-5 h-5" name="" id="" />
+          <input
+            type="checkbox"
+            className="w-5 h-5"
+            onClick={handleSelectAll}
+          />
           <label htmlFor="">Select All</label>
         </div>
-        {array.map((filter) => (
+        {array.map((filter, index) => (
           <div className="flex items-center gap-2 ml-2 ">
-            <input type="checkbox" className="w-5 h-5" name="" id="" />
+            <input
+              type="checkbox"
+              className="w-5 h-5"
+              checked={checkboxes[index]}
+              onChange={() => handleCheckboxChange(index)}
+            />
 
             <label htmlFor="">{filter}</label>
           </div>

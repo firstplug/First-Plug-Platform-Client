4da17423451bdd52a62c47d5ab2f9b4c16265c79
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-09-26 18:15:23 -0300

    equipment table finished

---
+++ src/common/State.jsx
@@ -0,0 +1,22 @@
+export default function State({ message }) {
+  const states = [
+    { state: "CONFIRMATION PENDING", color: "bg-lightPurple" },
+    { state: "PAYMENT PENDING", color: "bg-lightYellow" },
+    { state: "ORDER CANCELED", color: "bg-lightRed" },
+    { state: "ORDER CONFIRMED", color: "bg-lightGreen" },
+    { state: "CLOSED", color: "bg-disabled" },
+    { state: "OPEN", color: "bg-lightBlue" },
+  ];
+
+  const stateObj = states.find((stateItem) => stateItem.state === message);
+  const backgroundColorClass = stateObj ? stateObj.color : "";
+
+  return (
+    <p
+      className={`${backgroundColorClass} py-1  rounded-full text-sm font-medium text-center whitespace-nowrap bg-disabled `}
+      style={{ width: `${message?.length + 1}ch` }}
+    >
+      {message}
+    </p>
+  );
+}

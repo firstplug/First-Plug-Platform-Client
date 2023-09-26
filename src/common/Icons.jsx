5c387119a19b873a3e012422a6d6cfe8e103c731
Braian Barrientos <braianbts@gmail.com>
2023-09-25 23:02:16 -0300

    view shop finished, add background color in tailwind config, icon and svg

--- src/common/Icons.jsx
+++ src/common/Icons.jsx
@@ -594,3 +594,22 @@ export function ChevronDown({ className }) {
     </svg>
   );
 }
+
+export function ChevronRight({ className, color }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={3}
+      stroke={`currentColor ${color || ""}`}
+      className={`w-6 h-6 ${className || ""}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+}

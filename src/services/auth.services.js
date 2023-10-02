7b345bf8e85184293ba19871190ce7e687655fda
Tute22 <agustinsandoval1601@gmail.com>
2023-10-02 18:14:20 -0300

    connected login in the backend

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 export class AuthServices {
+  static apiURL = "http://localhost:3001";
   static async register(data) {
-    const userData = await axios.post(
-      "http://localhost:3001/api/auth/register",
-      data
-    );
+    const userData = await axios.post(`${apiURL}/api/auth/register`, data);
+
+    return userData.data;
+  }
+
+  static async login(data) {
+    const userData = await axios.post(`${apiURL}/api/auth/login`, data);
 
     return userData.data;
   }

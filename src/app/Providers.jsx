80a6e81c129d18bda71d77f4fedcb60d384f018b
sebikap-gm2 <sebastiankaplanski@gmail.com>
2023-10-03 09:10:48 -0300

    feat: add provider with mock data

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,6 +1,22 @@
 "use client";
+import { RootStore, RootStoreContext } from "@/models/root.store";
 import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  // TODO: Hydrate the store from wherever neccesary.
+  // TODO: Remove mock data.
+  const store = RootStore.create({ ...mockData });
+
+  return (
+    <RootStoreContext.Provider value={store}>
+      <SessionProvider>{children}</SessionProvider>
+    </RootStoreContext.Provider>
+  );
 }
+
+const mockData = {
+  products: [
+    { _id: "test", title: "Product Test", description: "Desc", price: 10 },
+    { _id: "test2", title: "Product Test 2", description: "Desc 2", price: 20 },
+  ],
+};

af0d973ed05bd37817d339ca66eaa6076611f080
Braian Barrientos <braianbts@gmail.com>
2023-10-02 21:58:31 -0300

    new button common for fixed bug in table.jsx

---
+++ src/common/ButtonMyStock.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavButtonIcon } from "./Icons";
+
+const ButtonMyStock = ({ body, onClick }) => {
+  return (
+    <div>
+      {" "}
+      <button
+        className="px-4 py-2 text-sm font-medium text-gray-700 rounded-md bg-white flex items-center"
+        onClick={onClick}
+      >
+        <p className="font-inter text-lg font-bold pr-2">{body}</p>
+        <NavButtonIcon />
+      </button>
+    </div>
+  );
+};
+
+export default ButtonMyStock;

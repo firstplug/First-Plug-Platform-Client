dfa03f84692cd8441388ee7a2eb64eaba3eaabc7
Tute22 <agustinsandoval1601@gmail.com>
2023-10-04 18:23:20 -0300

    initial config for MobX

---
+++ src/models/root.store.js
@@ -0,0 +1,75 @@
+"use client";
+import { createContext, useContext } from "react";
+import { types } from "mobx-state-tree";
+
+const Products = types.model({
+  _id: types.string,
+  category: types.string,
+  model: types.string,
+  color: types.string,
+  screen: types.string,
+  keyboard: types.string,
+  processor: types.string,
+  ram: types.string,
+  storage: types.string,
+  gpu: types.string,
+  serialNumber: types.string,
+  price: types.string,
+  status: types.string,
+});
+
+const Orders = types.model({
+  _id: types.string,
+  teamMember: types.optional(types.array(types.string), []),
+  status: types.string,
+  orderDate: types.Date,
+  totalPrice: types.string,
+  products: types.optional(types.array(types.string), []),
+});
+
+const Shipments = types.model({
+  _id: types.string,
+  fullname: types.string,
+  date: types.Date,
+  QuantityProducts: types.string,
+  types: types.string,
+  trackingNumber: types.string,
+  trackingURL: types.string,
+  price: types.string,
+  orders: types.optional(types.array(types.string), []),
+});
+
+const TeamMember = types.model({
+  _id: types.string,
+  lastName: types.string,
+  dateOfBirth: types.Date,
+  phone: types.string,
+  DNI: types.string,
+  email: types.string,
+  jobPosition: types.string,
+  city: types.string,
+  zipcode: types.string,
+  address: types.string,
+  appartment: types.string,
+  joiningDate: types.Date,
+  timeSlotForDelivery: types.string,
+  additionalInfo: types.string,
+  teams: types.optional(types.array(types.string), []),
+});
+
+const Teams = types.model({
+  _id: types.string,
+  name: types.string,
+  teamMember: types.optional(types.array(types.string), []),
+});
+
+export const RootStore = types.model({
+  products: types.array(Products),
+  orders: types.array(Orders),
+  shipments: types.array(Shipments),
+  teamMember: types.array(TeamMember),
+  teams: types.array(Teams),
+});
+
+export const RootStoreContext = createContext(RootStore);
+export const useStore = () => useContext(RootStoreContext);
--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
+const apiURL = "http://localhost:3001";
+
 export class AuthServices {
-  static apiURL = "http://localhost:3001";
   static async register(data) {
     const userData = await axios.post(`${apiURL}/api/auth/register`, data);
-
     return userData.data;
   }
 
   static async login(data) {
     const userData = await axios.post(`${apiURL}/api/auth/login`, data);
+    return userData.data;
+  }
 
+  static async me() {
+    const userData = await axios.get(`${apiURL}/api/auth/login`);
     return userData.data;
   }
 }

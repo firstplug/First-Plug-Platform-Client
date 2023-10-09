3e2fe0799a3724c8a8135ea2a1a1adbc5753ad0c
Braian Barrientos <braianbts@gmail.com>
2023-10-09 09:59:14 -0300

    bugfix: modify the customLink's location for takes the button and not the icon

--- src/app/home/dashboard/page.jsx
+++ src/app/home/dashboard/page.jsx
@@ -8,39 +8,41 @@ export default function Dashboard() {
     <Layout>
       <div className="flex flex-col gap-4 w-full h-full">
         <section className="flex-1 h-full">
-          <Card
-            Title="My Team"
-            titleButton="Add Team Member"
-            imageBottom="/girl.svg"
-            altImage="My Team"
-            icon={
-              <CustomLink href="/home/dashboard/data">
-                <AddIcon />
-              </CustomLink>
-            }
-            paragraph="You haven't loaded any employees yet."
-            className="h-full"
-          />
+          <CustomLink href="/home/dashboard/data">
+            <Card
+              Title="My Team"
+              titleButton="Add Team Member"
+              imageBottom="/girl.svg"
+              altImage="My Team"
+              icon={<AddIcon />}
+              paragraph="You haven't loaded any employees yet."
+              className="h-full"
+            />
+          </CustomLink>
         </section>
         <section className="flex-1 grid grid-cols-2 gap-4">
-          <Card
-            Title="My Stock"
-            titleButton="Shop Now"
-            imageBottom="/office.svg"
-            altImage="My Stock"
-            icon={<ShopIcon />}
-            paragraph="You dont't have any items."
-            className="h-full"
-          />
-          <Card
-            Title={"Notifications"}
-            titleButton="Shop Now"
-            imageBottom="/alert.svg"
-            altImage="Notifications"
-            icon={<ShopIcon />}
-            paragraph="You dont't have any orders."
-            className="h-full"
-          />
+          <CustomLink href="/shop">
+            <Card
+              Title="My Stock"
+              titleButton="Shop Now"
+              imageBottom="/office.svg"
+              altImage="My Stock"
+              icon={<ShopIcon />}
+              paragraph="You dont't have any items."
+              className="h-full"
+            />
+          </CustomLink>
+          <CustomLink href="/shop">
+            <Card
+              Title={"Notifications"}
+              titleButton="Shop Now"
+              imageBottom="/alert.svg"
+              altImage="Notifications"
+              icon={<ShopIcon />}
+              paragraph="You dont't have any orders."
+              className="h-full"
+            />
+          </CustomLink>
         </section>
       </div>
     </Layout>

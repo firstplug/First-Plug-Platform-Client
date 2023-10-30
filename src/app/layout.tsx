1a4673008e0282474df6610840caaa3e0f3d7442
Braian Barrientos <braianbts@gmail.com>
2023-10-30 00:38:31 -0300

    feat: file name modification and add children type

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import "./globals.css";
+import { Montserrat, Inter } from "next/font/google";
+import Providers from "./Providers";
+import { ReactNode } from "react";
+
+const montserrat = Montserrat({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
+});
+
+const inter = Inter({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+export const metadata = {
+  title: "First-Plug-Platform",
+  description:
+    "Web platform focused on inventory management, acquisition and control.",
+};
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+
+export default function RootLayout({ children } :RootLayoutProps) {
+  return (
+    <html lang="en" className={`${inter.variable} ${montserrat.variable}`}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}

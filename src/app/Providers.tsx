a84162132c813e9ff5d1b73f1690e2ec6fd2294c
Braian Barrientos <braianbts@gmail.com>
2023-10-30 00:39:50 -0300

    chore: file name modification

---
+++ src/app/Providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { RootStoreContext, RootStore } from "@/models/root.store";
+import { SessionProvider } from "next-auth/react";
+import { useCallback, useEffect,  FC, createContext, ReactNode, ReactElement  } from "react";
+import { TeamMemberServices } from "@/services/teamMember.services";
+import { ProductServices } from "@/services/product.services";
+
+export default function Providers({ children }) {
+  const store = RootStore.create({
+    users: [],
+    products: [],
+    orders: [],
+    shipments: [],
+    teams: [],
+    members: [],
+  });
+
+  const setData = useCallback(async () => {
+    TeamMemberServices.getAllMembers().then((res) => {
+      store.setMembers(res.data);
+    });
+    ProductServices.getAllProducts().then((res) => {
+      store.setProducts(res);
+    });
+  }, [store]);
+
+  useEffect(() => {
+    setData();
+  }, [store]);
+
+  return (
+    <RootStoreContext.Provider value={store}>
+      <SessionProvider>{children}</SessionProvider>
+    </RootStoreContext.Provider>
+  );
+}

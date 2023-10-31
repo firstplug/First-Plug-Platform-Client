56044eac3d44e65b01bb9975225107e1d404cd20
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-10-31 19:50:54 -0300

    chore: setting environment variable to services

--- src/services/orders.services.js
+++ src/services/orders.services.js
@@ -1,28 +1,39 @@
 import axios from "axios";
-const apiUrl = "http://localhost:3001/api";
 
 export class OrderServices {
   static async getAllOrders() {
-    const response = await axios.get(`${apiUrl}/orders`);
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/orders`
+    );
     return response.data;
   }
 
   static async createOrder(data) {
-    const response = await axios.post(`${apiUrl}/orders`, data);
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/orders`,
+      data
+    );
     return response.data;
   }
   static async updateOrder(id, data) {
-    const response = await axios.put(`${apiUrl}/orders/${id}`, data);
+    const response = await axios.put(
+      `${process.env.NEXT_PUBLIC_API_URL}/orders/${id}`,
+      data
+    );
     return response.data;
   }
 
   static async deleteOrder(id) {
-    const response = await axios.delete(`${apiUrl}/orders/${id}`);
+    const response = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_URL}/orders/${id}`
+    );
     return response.data;
   }
 
   static async getOneOrder(orderId) {
-    const response = await axios.get(`${apiUrl}/orders/${orderId}`);
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/orders/${orderId}`
+    );
     return response.data;
   }
 }
--- src/services/team.services.js
+++ src/services/team.services.js
@@ -1,34 +1,46 @@
 import axios from "axios";
-const apiURL = "http://localhost:3001/api";
 
 export class TeamServices {
   static async getAllTeams() {
-    const response = await axios.get(`${apiURL}/teams`);
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/teams`
+    );
     return response.data;
   }
 
   static async createTeam(teamData) {
-    const response = await axios.post(`${apiURL}/teams`, teamData);
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/teams`,
+      teamData
+    );
     return response.data;
   }
 
   static async updateTeam(id, teamData) {
-    const response = await axios.put(`${apiURL}/teams/${id}`, teamData);
+    const response = await axios.put(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/teams/${id}`,
+      teamData
+    );
     return response.data;
   }
 
   static async deleteTeam(id) {
-    const response = await axios.delete(`${apiURL}/teams/${id}`);
+    const response = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/teams/${id}`
+    );
     return response.data;
   }
 
   static async deleteFromTeam(teamId, memberId) {
     return await axios.delete(
-      `${apiURL}/teams/deleteMember/${teamId}/${memberId}`
+      `${process.env.NEXT_PUBLIC_API_URL}/api/teams/deleteMember/${teamId}/${memberId}`
     );
   }
 
   static async addToTeam(teamId, memberId) {
-    return await axios.post(`${apiURL}/teams/addTeam`, { teamId, memberId });
+    return await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/teams/addTeam`,
+      { teamId, memberId }
+    );
   }
 }

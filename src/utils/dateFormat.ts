1dfd8cebb4d3f06dea7843659666d6efe326ba41
Braian Barrientos <braianbts@gmail.com>
2023-10-31 18:03:21 -0300

    fix: correction params type in function

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,4 +1,4 @@
-export function dateTo_DDMMYY(date: Date) : string {
+export function dateTo_DDMMYY(date: string) : string {
   const fecha = new Date(date);
   const dia = fecha.getDate().toString().padStart(2, "0");
   const mes = (fecha.getMonth() + 1).toString().padStart(2, "0"); // Los meses van de 0 a 11

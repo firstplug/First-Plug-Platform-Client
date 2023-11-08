1bb0c641d4dc8bd1035fb61c1c670245ff966ae0
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-08 14:33:31 -0300

    fix(Team Members): fixed component teamMembers

--- src/components/TeamMembers.tsx
+++ src/components/TeamMembers.tsx
@@ -5,48 +5,16 @@ import GridTeam from "./GridTeam";
 import { useStore } from "@/models/root.store";
 
 interface TeamMembersProps {
-  display?: string,
+  display?: string;
 }
 
-interface MemberProps {
-  _id: string;
-  firstName: string;
-  lastName: string;
-  dateOfBirth: string;
-  joiningDate: string;
-  teams: string[]; 
-  jobPosition: string; 
-  shimentsDetails?: string;
-}
-
-export default observer(function TeamMembers({ display } : TeamMembersProps) {
-  const store = useStore();
-
-  const members: MemberProps[] = store.members.map((member) => ({
-    _id: member._id,
-    firstName: member.firstName,
-    lastName: member.lastName,
-    dateOfBirth: member.dateOfBirth,
-    phone: member.phone,
-    email: member.email,
-    jobPosition: member.jobPosition,
-    city: member.city,
-    zipCode: member.zipCode,
-    address: member.address,
-    appartment: member.appartment,
-    joiningDate: member.joiningDate,
-    timeSlotForDelivery: member.timeSlotForDelivery,
-    additionalInfo: member.additionalInfo,
-    teams: member.teams,
-  }));
-
+export default observer(function TeamMembers({ display }: TeamMembersProps) {
+  const {
+    members: { members },
+  } = useStore();
   return (
     <section className="flex flex-col gap-4">
-      {display === "grid" ? (
-        <GridTeam members={members} />
-      ) : (
-        <TableTeam members={members} />
-      )}
+      {display === "grid" ? <GridTeam members={members} /> : <TableTeam />}
     </section>
   );
 });

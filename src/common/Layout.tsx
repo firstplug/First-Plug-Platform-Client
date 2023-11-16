1fe093ccc7b29f647f725a8983bf8df0228d1803
Braian Barrientos <braianbts@gmail.com>
2023-11-16 10:31:30 -0300

    feat: default was deleted

--- src/common/Layout.tsx
+++ src/common/Layout.tsx
@@ -5,7 +5,7 @@ interface LayoutProps  {
   className?: string;
 };
 
-export default function Layout({ children, className }: LayoutProps) {
+export function Layout({ children, className }: LayoutProps) {
   return (
     <section
       className={`h-[calc(98vh-100px)] w-[96%] mx-auto py-6 mt-6 ${className}`}

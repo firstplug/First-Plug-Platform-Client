8f66461b331041045b5ea9c2f4f40a6bbdb3cfe8
Braian Barrientos <braianbts@gmail.com>
2023-11-16 10:31:38 -0300

    feat: default was deleted

--- src/common/LogisticsRow.tsx
+++ src/common/LogisticsRow.tsx
@@ -1,4 +1,4 @@
-import State from "./State";
+import {State} from "./State";
 
 interface LogisticsRowProps{
   month: string;
@@ -8,7 +8,7 @@ interface LogisticsRowProps{
   className?: string;
 }
 
-export default function LogisticsRow({
+export function LogisticsRow({
   month,
   shipmentQuantity,
   state,

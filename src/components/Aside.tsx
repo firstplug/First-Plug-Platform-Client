b10c70e1df81347d645c474ad83630b80ce8cc7d
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-19 18:31:25 -0300

    chore (Aside): setting aside open or close white the status of "type"

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -6,13 +6,15 @@ import { useStore } from "@/models/root.store";
 import AsideTitle from "@/common/AsideTitle";
 
 interface AsideProps {
-  className?: string
+  className?: string;
 }
 
 export default observer(function Aside({ className = "" }: AsideProps) {
-  const { aside: { closeAside, isOpen } } = useStore();
+  const {
+    aside: { type, setAside },
+  } = useStore();
 
-  return isOpen ? (
+  return type ? (
     <>
       {/* overlay */}
       <div
@@ -28,7 +30,7 @@ export default observer(function Aside({ className = "" }: AsideProps) {
           <h2 className="text-2xl font-sans text-black font-semibold">
             <AsideTitle />
           </h2>
-          <button onClick={() => closeAside()}>
+          <button onClick={() => setAside(undefined)}>
             <IconX className="h-8 w-8" />
           </button>
         </header>

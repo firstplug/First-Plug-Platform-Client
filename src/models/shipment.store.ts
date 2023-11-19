2b4584f515b0873fc169c1d96db229b231caa2df
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-19 18:18:37 -0300

    feat (shipmentStore): adding views & actions

--- src/models/shipment.store.ts
+++ src/models/shipment.store.ts
@@ -1,6 +1,22 @@
 import { types } from "mobx-state-tree";
 import { ShimpentModel } from "@/types";
 
-export const ShipmentStore = types.model({
-  shipments: types.array(ShimpentModel),
-});
+export const ShipmentStore = types
+  .model({
+    shipments: types.array(ShimpentModel),
+    shipmentId: types.optional(types.string, ""),
+  })
+  .views((store) => ({
+    get selectedShipment() {
+      return store.shipments.find(
+        (shipment) => shipment._id === store.shipmentId
+      );
+    },
+    get shipmentDetails() {
+      const shipment = store.shipments.find(
+        (shipment) => shipment._id === store.shipmentId
+      );
+
+      return shipment.products;
+    },
+  }));

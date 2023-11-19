a406628f149f2c0dd68241ffb3f608b7da37df3e
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-19 18:19:21 -0300

    feat (productStore):  adding viea to uniqueProducts

--- src/models/products.store.ts
+++ src/models/products.store.ts
@@ -6,6 +6,18 @@ export const ProductsStore = types
   .model({
     products: types.array(ProductModel),
   })
+  .views((store) => ({
+    get uniqueProducts() {
+      const groupedProducts = store.products.reduce((result, product) => {
+        if (!result[product.category]) {
+          result[product.category] = product;
+        }
+        return result;
+      }, {});
+
+      return Object.values(groupedProducts) as Product[];
+    },
+  }))
   .actions((store) => ({
     setProducts(products: Product[]) {
       store.products.push(...products);

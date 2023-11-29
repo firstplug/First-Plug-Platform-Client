26bb4386d3f0a630a4549441eaaec2f6bf587c5e
Braian Barrientos <braianbts@gmail.com>
2023-11-29 10:20:34 -0300

    chore(ShipmentStatus): deleted comments

--- src/common/ShipmentStatus.tsx
+++ src/common/ShipmentStatus.tsx
@@ -7,13 +7,11 @@ interface ShipmentStatusProps {
 }
 
 export function ShipmentStatus({ status }: ShipmentStatusProps) {
-  // TODO: remove from here "status" and move it to types carpet that is doing to Fran
 
   return (
     <span className="flex items-center gap-1 font-light">
       <StatusCircleIcon />
       {status}
-      {/* {status.slice(0, 1).toUpperCase().concat(status.slice(1))} */}
     </span>
   );
 }

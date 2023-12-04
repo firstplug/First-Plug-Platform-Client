229dd230e73615fb6cec9b4225ea2f87c52c3847
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-04 11:35:30 -0300

    fix (settings): fixed billing form styles

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -4,40 +4,36 @@ interface Props {
 }
 export const BillingForm = function ({ handleInput }: Props) {
   return (
-    <Card Title="Billing Information" className="flex flex-col gap-4 px-8 pb-8">
-      <div className="flex gap-4">
-        <div className="w-1/4 flex flex-col gap-4">
-          <FormInput
-            type="text"
-            prop="country"
-            placeholder="Country"
-            handleInput={handleInput}
-            title="Country"
-          />
-          <FormInput
-            placeholder="Apparment, Suite, etc."
-            title="Apparment, Suite, etc."
-            type="text"
-            prop="appartment"
-            handleInput={handleInput}
-          />
-        </div>
-        <div className="w-1/4 flex flex-col gap-4">
-          <FormInput
-            placeholder="City"
-            title="City"
-            type="text"
-            prop="city"
-            handleInput={handleInput}
-          />
-          <FormInput
-            placeholder="Address"
-            title="Address"
-            type="text"
-            prop="address"
-            handleInput={handleInput}
-          />
-        </div>
+    <Card Title="Billing Information">
+      <div className="grid grid-cols-4 gap-4">
+        <FormInput
+          type="text"
+          prop="country"
+          placeholder="Country"
+          handleInput={handleInput}
+          title="Country"
+        />
+        <FormInput
+          placeholder="Apparment, Suite, etc."
+          title="Apparment, Suite, etc."
+          type="text"
+          prop="appartment"
+          handleInput={handleInput}
+        />
+        <FormInput
+          placeholder="City"
+          title="City"
+          type="text"
+          prop="city"
+          handleInput={handleInput}
+        />
+        <FormInput
+          placeholder="Address"
+          title="Address"
+          type="text"
+          prop="address"
+          handleInput={handleInput}
+        />
         <FormInput
           placeholder="State"
           title="State"

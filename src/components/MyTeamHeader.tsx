1d3d360d22b1403be9a7d17ad507ce73708ae50e
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-11 12:46:34 -0300

    fixed key prop on myTeamHeader

--- src/components/MyTeamHeader.tsx
+++ src/components/MyTeamHeader.tsx
@@ -8,7 +8,7 @@ export function MyTeamHeader({ members }: { members: TeamMember[] }) {
       <h1 className="text-2xl text-black font-semibold">My Team</h1>
       <div className="flex">
         {members.slice(0, 3).map((member) => (
-          <CircleMemberImg member={member} />
+          <CircleMemberImg member={member} key={member._id} />
         ))}
         {members.length > 2 && (
           <div className="h-[3rem] w-[3rem] grid place-items-center bg-white rounded-full -ml-[1.75rem] border-white border z-20 ">

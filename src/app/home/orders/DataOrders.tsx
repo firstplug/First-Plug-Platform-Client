1c0ecff6cf06e6ba7116d52889d33ce53b33bb78
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-24 20:39:31 +0800

    chore (orders): refactor orders view

---
+++ src/app/home/orders/DataOrders.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+import { TableEquipment, TableLogistics } from "@/components";
+import { Layout, Header } from "@/common";
+import { useStore } from "@/models";
+import { OrderServices } from "@/services";
+export default function DataOrders() {
+    const [selectedTab, setSelectedTab] = useState<"Logistics" | "Equipment">("Equipment");
+    const { orders } = useStore();
+    const handleTabClick = (tabName: "Logistics" | "Equipment") => {
+      setSelectedTab(tabName);
+    };
+    useEffect(() => {
+        OrderServices.getAllOrders().then((res) => {
+          orders.setOrders(res);
+        });
+      }, [orders]);
+  
+    return (
+      <Layout className="flex flex-col gap-8">
+        <Header selectedTab={selectedTab} handleTab={handleTabClick} />
+        {selectedTab === "Logistics" ? 
+    
+        <TableLogistics />
+        :<TableEquipment />
+        }
+      </Layout>
+    );
+}

6d07d2063160211cb44b085940b46d54ef0c0328
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-27 12:19:09 -0300

    fix (dashboard): atomic logic of empty cards

--- src/app/home/dashboard/EmptyDashboard.tsx
+++ src/app/home/dashboard/EmptyDashboard.tsx
@@ -7,7 +7,6 @@ export default function EmptyDashboard() {
     <Layout>
       <div className="flex flex-col gap-4 w-full h-full">
         <section className="flex-1 h-full">
-          <CustomLink href="/home/dashboard/data">
             <Card
               Title="My Team"
               titleButton="Add Team Member"
@@ -17,10 +16,8 @@ export default function EmptyDashboard() {
               paragraph="You haven't loaded any employees yet."
               className="h-full"
             />
-          </CustomLink>
         </section>
         <section className="flex-1 grid grid-cols-2 gap-4">
-          <CustomLink href="/shop">
             <Card
               Title="My Stock"
               titleButton="Shop Now"
@@ -30,8 +27,6 @@ export default function EmptyDashboard() {
               paragraph="You dont't have any items."
               className="h-full"
             />
-          </CustomLink>
-          <CustomLink href="/shop">
             <Card
               Title={"Notifications"}
               titleButton="Shop Now"
@@ -41,7 +36,6 @@ export default function EmptyDashboard() {
               paragraph="You dont't have any orders."
               className="h-full"
             />
-          </CustomLink>
         </section>
       </div>
     </Layout>
--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -1,18 +1,63 @@
 "use client"
 import { observer } from "mobx-react-lite";
-import DataDashboard from "./DataDashboard";
-import EmptyDashboard from "./EmptyDashboard";
 import { useStore } from "@/models";
+import { AddIcon, Layout, ShopIcon } from "@/common";
+import { Card, StockCard, TeamHomeCard } from "@/components";
 
 export default observer( function Dashboard() {
   //TODO: REVIEW ==> En esta vista en particular, se necesita la data de members y stock.
   const {members:{members}, products:{products}} = useStore()
 
-  console.log({members, products})
-  return <div>
-    {(members.length && products.length) 
-    ?  <DataDashboard />
-      :<EmptyDashboard/>
-    }
-    </div>
+  
+  return   <Layout>
+  <div className="flex flex-col gap-4 w-full h-full">
+    <section className="flex-1 h-full">
+      {
+        members.length ?
+        <Card className="flex-1 h-1/2">
+          <TeamHomeCard/>
+        </Card>:
+        <Card
+        Title="My Team"
+        titleButton="Add Team Member"
+        imageBottom="/girl.svg"
+        altImage="My Team"
+        icon={<AddIcon />}
+        paragraph="You haven't loaded any employees yet."
+        className="h-full"
+        />
+        
+        }
+    </section>
+    <section className="flex-1 grid grid-cols-2 gap-4">
+        {products.length ? 
+          <Card
+            Title="My Stock"
+            titleButton="Shop Now"
+            icon={<ShopIcon />}
+          >
+            <StockCard className="my-4" />
+          </Card> : 
+          <Card
+          Title="My Stock"
+          titleButton="Shop Now"
+          imageBottom="/office.svg"
+          altImage="My Stock"
+          icon={<ShopIcon />}
+          paragraph="You dont't have any items."
+          className="h-full"
+        />}
+
+        
+
+         <Card
+            Title="Notifications"
+            imageBottom="/alert.svg"
+            icon={<ShopIcon />}
+            paragraph="You dont't have any orders."
+            className="h-full"
+          />
+    </section>
+  </div>
+</Layout>
 })

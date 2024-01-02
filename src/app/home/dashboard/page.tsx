181217ffaa3dd962e787d1d75c55df4748ba7ba1
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-01-02 18:28:12 -0300

    chore (code): cleaning code

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -8,25 +8,22 @@ export default observer( function Dashboard() {
   //TODO: REVIEW ==> En esta vista en particular, se necesita la data de members y stock.
   const {members:{members}, products:{products}} = useStore()
 
-  
   return   <Layout>
   <div className="flex flex-col gap-4 w-full h-full">
     <section className="flex-1 h-full">
-      {
-        members.length ?
-        <Card className="flex-1 h-1/2">
-          <TeamHomeCard/>
-        </Card>:
-        <Card
-        Title="My Team"
-        titleButton="Add Team Member"
-        imageBottom="/girl.svg"
-        altImage="My Team"
-        icon={<AddIcon />}
-        paragraph="You haven't loaded any employees yet."
-        className="h-full"
-        />
-        
+        {members.length?
+          <Card className="flex-1 h-1/2">
+            <TeamHomeCard/>
+          </Card>
+        : <Card
+            Title="My Team"
+            titleButton="Add Team Member"
+            imageBottom="/girl.svg"
+            altImage="My Team"
+            icon={<AddIcon />}
+            paragraph="You haven't loaded any employees yet."
+            className="h-full"
+            />
         }
     </section>
     <section className="flex-1 grid grid-cols-2 gap-4">
@@ -37,26 +34,25 @@ export default observer( function Dashboard() {
             icon={<ShopIcon />}
           >
             <StockCard className="my-4" />
-          </Card> : 
-          <Card
-          Title="My Stock"
-          titleButton="Shop Now"
-          imageBottom="/office.svg"
-          altImage="My Stock"
-          icon={<ShopIcon />}
-          paragraph="You dont't have any items."
-          className="h-full"
-        />}
-
-        
-
-         <Card
-            Title="Notifications"
-            imageBottom="/alert.svg"
-            icon={<ShopIcon />}
-            paragraph="You dont't have any orders."
-            className="h-full"
-          />
+          </Card> 
+          :<Card
+              Title="My Stock"
+              titleButton="Shop Now"
+              imageBottom="/office.svg"
+              altImage="My Stock"
+              icon={<ShopIcon />}
+              paragraph="You dont't have any items."
+              className="h-full"
+            />
+          }
+         {/* TODO: Cuál es la data de notificaciones? */}
+           <Card
+              Title="Notifications"
+              imageBottom="/alert.svg"
+              icon={<ShopIcon />}
+              paragraph="You dont't have any orders."
+              className="h-full"
+            />
     </section>
   </div>
 </Layout>

6b6f732c078eda8f1567f66c006c9b4cd1f64df0
Braian Barrientos <braianbts@gmail.com>
2024-01-02 17:54:52 -0300

    fix(deploy): fix team services to deploy

--- src/services/team.services.ts
+++ src/services/team.services.ts
@@ -1,62 +1,62 @@
-// import { TeamMember, Team } from "@/types";
-// import axios, { AxiosResponse } from "axios";
+import { TeamMember, Team } from "@/types";
+import axios, { AxiosResponse } from "axios";
 
-// const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-// type TeamCreation = Omit<Team, "_id" | "__v">;
+type TeamCreation = Omit<Team, "_id" | "__v">;
 
-// export class TeamServices {
-//   static async getAllTeams(): Promise<Team[]> {
-//     const response: AxiosResponse = await axios.get(`${BASE_URL}/api/teams`);
-//     return response.data;
-//   }
+export class TeamServices {
+  static async getAllTeams(): Promise<Team[]> {
+    const response: AxiosResponse = await axios.get(`${BASE_URL}/api/teams`);
+    return response.data;
+  }
 
-//   static async createTeam(teamData: TeamCreation): Promise<Team> {
-//     const response: AxiosResponse = await axios.post(
-//       `${BASE_URL}/api/teams`,
-//       teamData
-//     );
-//     return response.data;
-//   }
+  static async createTeam(teamData: TeamCreation): Promise<Team> {
+    const response: AxiosResponse = await axios.post(
+      `${BASE_URL}/api/teams`,
+      teamData
+    );
+    return response.data;
+  }
 
-//   static async updateTeam(
-//     id: Team["_id"],
-//     teamData: TeamCreation
-//   ): Promise<Team> {
-//     const response: AxiosResponse = await axios.put(
-//       `${BASE_URL}/api/teams/${id}`,
-//       teamData
-//     );
-//     return response.data;
-//   }
+  static async updateTeam(
+    id: Team["_id"],
+    teamData: TeamCreation
+  ): Promise<Team> {
+    const response: AxiosResponse = await axios.put(
+      `${BASE_URL}/api/teams/${id}`,
+      teamData
+    );
+    return response.data;
+  }
 
-//   static async deleteTeam(id: Team["_id"]): Promise<Team> {
-//     const response: AxiosResponse = await axios.delete(
-//       `${BASE_URL}/api/teams/${id}`
-//     );
-//     return response.data;
-//   }
+  static async deleteTeam(id: Team["_id"]): Promise<Team> {
+    const response: AxiosResponse = await axios.delete(
+      `${BASE_URL}/api/teams/${id}`
+    );
+    return response.data;
+  }
 
-//   static async deleteFromTeam(
-//     teamId: Team["_id"],
-//     memberId: TeamMember["_id"]
-//   ): Promise<Team> {
-//     return await axios.delete(
-//       `${BASE_URL}/api/teams/deleteMember/${teamId}/${memberId}`
-//     );
-//   }
+  static async deleteFromTeam(
+    teamId: Team["_id"],
+    memberId: TeamMember["_id"]
+  ): Promise<Team> {
+    return await axios.delete(
+      `${BASE_URL}/api/teams/deleteMember/${teamId}/${memberId}`
+    );
+  }
 
-//   static async addToTeam(
-//     teamId: Team["_id"],
-//     memberId: TeamMember["_id"]
-//   ): Promise<Team> {
-//     const response: AxiosResponse = await axios.post(
-//       `${BASE_URL}/api/teams/addTeam`,
-//       {
-//         teamId,
-//         memberId,
-//       }
-//     );
-//     return response.data;
-//   }
-// }
+  static async addToTeam(
+    teamId: Team["_id"],
+    memberId: TeamMember["_id"]
+  ): Promise<Team> {
+    const response: AxiosResponse = await axios.post(
+      `${BASE_URL}/api/teams/addTeam`,
+      {
+        teamId,
+        memberId,
+      }
+    );
+    return response.data;
+  }
+}

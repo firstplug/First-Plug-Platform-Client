c1b6897a3dbd74f1cb75f38197d858ba5fd18fd0
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-02-21 10:58:49 -0300

    fix (memebers): fixed conditional view of my team page

--- src/app/home/my-team/page.tsx
+++ src/app/home/my-team/page.tsx
@@ -8,36 +8,11 @@ import EmptyTeam from "./EmptyTeam";
 export default function MyTeam() {
   const {
     aside: { setAside },
-    
+    members: { members },
   } = useStore();
   return (
-    <Layout className="border-2 shadow-sm border-border rounded-md grid place-items-center w-[98%] ">
-      <EmptyCard
-        imageBottom="/girl.svg"
-        paragraph="You havnet't loaded any  employees yet."
-        altImage="Girl"
-      >
-        <div className="flex gap-2 ">
-          <Button
-            body="Load Team Members"
-            icon={<AddIcon />}
-            onClick={() => {
-              setAside("LoadStock", "MyStock");
-            }}
-            variant="secondary"
-            size="big"
-            className="rounded-md"
-          />
-          <CustomLink
-            variant="primary"
-            size="big"
-            className="rounded-md flex gap-2"
-            href="#"
-          >
-            <UpLoadIcon /> Add Team Memeber
-          </CustomLink>
-        </div>
-      </EmptyCard>
+    <Layout className="border shadow-sm border-border rounded-md grid place-items-center w-[98%] ">
+      {members.length ? <DataTeam /> : <EmptyTeam />}
     </Layout>
   );
 }

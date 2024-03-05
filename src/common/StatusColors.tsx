8637f43b1f58094b33d74418e96d12ee9d14bc30
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-03-05 00:14:44 -0300

    chore (statusColors): refactor status colors

--- src/common/StatusColors.tsx
+++ src/common/StatusColors.tsx
@@ -1,3 +1,5 @@
+import { OrderStatus } from "@/types";
+
 export const StatusColors = {
   pending: "bg-lightPurple",
   info: "bg-lightBlue",
@@ -9,34 +11,14 @@ export const StatusColors = {
 
 export type StatusColor = keyof typeof StatusColors;
 
-export type OrderStatus =
-  | "CONFIRMATION PENDING"
-  | "PAYMENT PENDING"
-  | "ORDER CANCELED"
-  | "ORDER CONFIRMED"
-  | "CLOSED"
-  | "OPEN"
-  | "DELIVERED"
-  | "MISSING DATA"
-  | "PREPARING"
-  | "AVAILABLE"
-  | "SHIPPED";
-
 export const StateColors: Record<OrderStatus, StatusColor> = {
-  "CONFIRMATION PENDING": "pending",
-  "PAYMENT PENDING": "warn",
-  "ORDER CANCELED": "warn",
-  "ORDER CONFIRMED": "success",
-  CLOSED: "disabled",
-  OPEN: "success",
-  DELIVERED: "success",
-  "MISSING DATA": "warn",
-  PREPARING: "warn",
-  AVAILABLE: "pending",
-  SHIPPED: "success",
+  ConfirmationPending: "pending",
+  PaymentPending: "warn",
+  Canceled: "warn",
+  Confirmed: "success",
 } as const;
 
-export const JOB_POSITION_COLORS = [
+export const JobPositionColors: string[] = [
   "bg-pink-400",
   "bg-green",
   "bg-purple",
@@ -44,24 +26,4 @@ export const JOB_POSITION_COLORS = [
   "bg-red-200",
 ];
 
-export type JobPosition = (typeof JOB_POSITION_COLORS)[number];
-
-export function mapStatusToOrderStatus(
-  status: string
-): OrderStatus | undefined {
-  const statusMap: Record<string, OrderStatus> = {
-    "Confirmation Pending": "CONFIRMATION PENDING",
-    "Payment Pending": "PAYMENT PENDING",
-    "Order Canceled": "ORDER CANCELED",
-    "Order Confirmed": "ORDER CONFIRMED",
-    Closed: "CLOSED",
-    Open: "OPEN",
-    Delivered: "DELIVERED",
-    "Missing Data": "MISSING DATA",
-    Preparing: "PREPARING",
-    Available: "AVAILABLE",
-    Shipped: "SHIPPED",
-  };
-
-  return statusMap[status] || undefined;
-}
+export type JobPosition = keyof typeof JobPositionColors;

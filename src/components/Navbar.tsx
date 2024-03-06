da004787450dd2964f8e847e722c37c76864c790
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-03-06 13:29:39 -0300

    chore(navbar): add title to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,30 +3,39 @@ import React from "react";
 import { Button, SearchInput, DropdownButton, ImgPorfile } from "@/common";
 import Image from "next/image";
 import Logo from "../../public/logo1.png";
-import { ShopIcon, NotificationIcon } from "@/common/Icons";
-import { useRouter } from "next/navigation";
+import { ShopIcon } from "@/common/Icons";
+import { usePathname, useRouter } from "next/navigation";
 
 type NavbarProps = {
   title?: string;
   searchInput?: string;
   placeholder?: string;
-  hasNotification?: boolean;
 };
 
 export const Navbar = function ({
   title,
   searchInput,
   placeholder,
-  hasNotification,
 }: NavbarProps) {
   const router = useRouter();
+  const pathName = usePathname();
+  const checkPath = (path: string) => pathName.split("/").includes(path);
+  const TITLE = checkPath("shipments")
+    ? "Shipments"
+    : checkPath("my-stock")
+    ? "My Stock"
+    : checkPath("my-team")
+    ? "My Team"
+    : checkPath("orders")
+    ? "Orders"
+    : "";
   return (
     <nav className="flex justify-between items-center h-[8vh] px-4  ">
       <div className="flex gap-6 items-center">
         {title === "logo" ? (
           <Image src={Logo} alt="Logo" width={140} height={300} priority />
         ) : (
-          <h1 className="font-bold text-2xl text-black">{title || ""}</h1>
+          <h1 className="font-bold text-2xl text-black">{TITLE}</h1>
         )}
 
         {searchInput && <SearchInput placeholder={placeholder} />}

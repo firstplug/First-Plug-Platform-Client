31f835f210b9d52811f826deb005f6c47bf70aa9
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-04-09 13:28:52 -0300

    fix (member): fix model & hardcode of products quantity

--- src/types/member.ts
+++ src/types/member.ts
@@ -1,5 +1,6 @@
 import { Instance, types } from "mobx-state-tree";
 import { ShipmentStatus } from "./shipment";
+import { ProductModel } from "./product";
 
 export const TeamMemberModel = types.model({
   _id: types.optional(types.string, ""),
@@ -18,6 +19,7 @@ export const TeamMemberModel = types.model({
   timeSlotForDelivery: types.optional(types.string, ""),
   additionalInfo: types.optional(types.string, ""),
   teams: types.optional(types.array(types.string), []),
+  products: types.optional(types.array(ProductModel), []),
 });
 
 export type TeamMember = Instance<typeof TeamMemberModel>;

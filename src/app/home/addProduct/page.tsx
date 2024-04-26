6cb847f099c30246e57d6c88819f9680d251b3f9
MercedesPoisson <mercedespoisson@yahoo.com>
2024-04-26 12:56:36 -0300

    .

---
+++ src/app/home/addProduct/page.tsx
@@ -0,0 +1,9 @@
+import { ProductsCreateForm } from "@/components/ProductsForm/ProductsCreateForm";
+
+export default function AddProduct() {
+  return (
+    <div>
+      <ProductsCreateForm />
+    </div>
+  );
+}
--- src/components/ProductsForm/ProductsFormBase.tsx
+++ src/components/ProductsForm/ProductsFormBase.tsx
@@ -1,14 +1,14 @@
+"use client";
 import { observer } from "mobx-react-lite";
 import { useStore } from "@/models/root.store";
 import { useEffect, useState } from "react";
-
-
+import { FormLayout, PageLayout, SectionTitle } from "@/common";
 interface ProductsFormBaseProps {
   onsubmit: (e: React.FormEvent<HTMLFormElement>) => void;
   buttonText: string;
   register: any;
   formErrors: any;
-    watch: any;
+  watch: any;
 }
 
 export const ProductsFormBase = observer(function ({
@@ -29,34 +29,97 @@ export const ProductsFormBase = observer(function ({
   }, [products.uniqueProducts]);
 
   return (
-    <div>
-      <form onSubmit={onsubmit}>
-        <select
-          {...register("category", {
-            required: "puedo poner el mensaje de validación",
-            maxLength: {
-              value: 20,
-              message: "Aca el mensaje que quiero que aparezca",
-            },
-            placeholder: "Category",
-          })}
-          defaultValue=""
-        >
-          <option value="" disabled>
-            Select a Category
-          </option>
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-          <p>{error.category?.message}</p>
-        </select>
-        <input {...register("name")} type="text" placeholder="Product Name">
-            {error.name?.message}
-        </input>
-        <button type="submit">{buttonText}</button>
-      </form>
+    <div className="relative h-full   w-full  ">
+      <div className=" absolute max-h-[90%] h-[90%] w-full   ">
+        <div className=" px-10 py-4 rounded-3xl  border  ">
+          <SectionTitle className="text-[20px]">Add Product</SectionTitle>
+          
+          <section className="flex flex-col gap-4 ">
+            <div className="  flex items-center gap-7  ">
+              <section className="  w-full ">
+                <FormLayout>
+              <select
+                {...register("category", {
+                  required: "puedo poner el mensaje de validación",
+                  maxLength: {
+                    value: 20,
+                    message: "Aca el mensaje que quiero que aparezca",
+                  },
+                  placeholder: "Category",
+                })}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Select a Category
+                </option>
+                {categories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+                <p>{error.category?.message}</p>
+              </select>
+              <input
+                {...register("name")}
+                type="text"
+                placeholder="Product Name"
+              >
+                {error.name?.message}
+              </input>
+              <button type="submit">{buttonText}</button>
+            </FormLayout>
+              </section>
+              
+            </div>
+            
+          </section>
+        </div>
+      </div>
     </div>
   );
 });
+
+//     <PageLayout>
+//       <div className="relative h-full   w-full  ">
+//         <div className=" absolute max-h-[90%] h-[90%] w-full overflow-y-auto   ">
+//           <div className=" px-10 py-4 rounded-3xl  border  ">
+//             <SectionTitle className="text-[20px]">Add Product</SectionTitle>
+//             <section>
+//               <FormLayout>
+//                 <select
+//                   {...register("category", {
+//                     required: "puedo poner el mensaje de validación",
+//                     maxLength: {
+//                       value: 20,
+//                       message: "Aca el mensaje que quiero que aparezca",
+//                     },
+//                     placeholder: "Category",
+//                   })}
+//                   defaultValue=""
+//                 >
+//                   <option value="" disabled>
+//                     Select a Category
+//                   </option>
+//                   {categories.map((category) => (
+//                     <option key={category} value={category}>
+//                       {category}
+//                     </option>
+//                   ))}
+//                   <p>{error.category?.message}</p>
+//                 </select>
+//                 <input
+//                   {...register("name")}
+//                   type="text"
+//                   placeholder="Product Name"
+//                 >
+//                   {error.name?.message}
+//                 </input>
+//                 <button type="submit">{buttonText}</button>
+//               </FormLayout>
+//             </section>
+//           </div>
+//         </div>
+//       </div>
+//     </PageLayout>
+//   );
+// });

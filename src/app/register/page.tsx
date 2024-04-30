6a8493884c032f920c252b8572e88de1c593ef52
MercedesPoisson <mercedespoisson@yahoo.com>
2024-04-30 12:14:47 -0300

    Redirect to "/register/success" on successful registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -39,7 +39,7 @@ export default function Register() {
         action: <AlertCheck />,
         duration: 1500,
       });
-      router.push("/login");
+      router.push("/register/success");
     } catch (error) {
       console.error(error);
     } finally {
--- src/app/register/success/page.tsx
+++ src/app/register/success/page.tsx
@@ -1,9 +1,16 @@
 import { EmptyCard, EmptyCardLayout } from "@/common";
+import { Navbar } from "@/components";
 
 export default function SuccessRegister() {
   return (
-    <EmptyCardLayout>
-      <EmptyCard type="registerok" />
-    </EmptyCardLayout>
+    <>
+      <Navbar title="logo" />
+
+      <div className="flex flex-col justify-center items-center  h-[100vh] gap-8 mx-[40px] my-[32px] border border-boder rounded-lg shadow-md">
+        <EmptyCardLayout>
+          <EmptyCard type="registerok" />
+        </EmptyCardLayout>
+      </div>
+    </>
   );
 }

98170205ad36388b8a2d638f6a0977736860f91b
MercedesPoisson <mercedespoisson@yahoo.com>
2024-05-10 14:40:24 -0300

    The location input depends on whether there is a member assigned or not.

--- src/components/AddProduct/CategoryForm.tsx
+++ src/components/AddProduct/CategoryForm.tsx
@@ -1,9 +1,10 @@
 "use Client";
-import React from "react";
+import React, { useState } from "react";
 import { DropdownInputProductForm } from "./DropDownProductForm";
 import { InputProductForm } from "./InputProductForm";
 import { useStore } from "@/models";
 import { observer } from "mobx-react-lite";
+import { Product, Location } from "@/types";
 
 const CategoryForm = function ({
   handleInput,
@@ -25,6 +26,24 @@ const CategoryForm = function ({
 
   const handleInputChange = (name: string, value: string) => {
     handleInput(name, value);
+  };
+
+  const [selectedFullName, setSelectedFullName] = useState<string>("");
+  const [selectedLocation, setSelectedLocation] = useState<string>("");
+
+  const handleAssignedMemberChange = (selectedFullName: string) => {
+    setSelectedFullName(selectedFullName);
+    const selectedMember = members.members.find(
+      (member) => `${member.firstName} ${member.lastName}` === selectedFullName
+    );
+    handleInput("assignedEmail", selectedMember?.email || "");
+    if (selectedFullName === "None") {
+      setSelectedLocation("Our Office");
+      handleInput("location", "Our Office");
+    } else {
+      setSelectedLocation("Employee");
+      handleInput("location", "Employee");
+    }
   };
 
   return (
@@ -83,27 +102,38 @@ const CategoryForm = function ({
             placeholder="Assigned Email"
             title="Assigned Member"
             name="assignedMember"
-            onChange={(selectedFullName) => {
-              const selectedMember = members.members.find(
-                (member) =>
-                  `${member.firstName} ${member.lastName}` === selectedFullName
-              );
-              handleInput("assignedEmail", selectedMember?.email || "");
-            }}
+            onChange={handleAssignedMemberChange}
             required="required"
             className="w-full "
           />
-          <InputProductForm
-            placeholder="Location"
-            title="Location"
-            type="text"
-            name="location"
-            prop="location"
-            handleInput={handleInputChange}
-            onChange={(e) => handleInputChange("location", e.target.value)}
-            required="required"
-            className="w-full "
-          />
+          {selectedFullName === "None" || selectedFullName === "" ? (
+            <DropdownInputProductForm
+              options={["Our Office", "FP Office"]}
+              placeholder="Location"
+              title="Location"
+              name="location"
+              selectedOption={selectedLocation}
+              onChange={(value: Location) => {
+                setSelectedLocation(value);
+                handleInput("location", value);
+              }}
+              required="required"
+              className="w-full"
+            />
+          ) : (
+            <InputProductForm
+              placeholder="Location"
+              title="Location"
+              type="text"
+              name="location"
+              prop="location"
+              value="Employee"
+              handleInput={handleInputChange}
+              onChange={(e) => handleInputChange("location", e.target.value)}
+              required="required"
+              className="w-full"
+            />
+          )}
         </div>
       </div>
     </>

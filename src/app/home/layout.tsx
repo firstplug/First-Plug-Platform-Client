b8f901c19f8a428c31e1112c19074deeae1b4ae6
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-05-13 15:38:34 -0300

    refactored home/layout with Data Provider

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,87 +1,24 @@
-"use client";
-import { ReactNode, useEffect, useState } from "react";
-import { Navbar, Sidebar, Aside, DownloadStock } from "@/components";
-import { useStore } from "@/models";
-import { observer } from "mobx-react-lite";
-import { useSession } from "next-auth/react";
-import { Memberservices, ProductServices } from "@/services";
-import { Layout, EmptyCard, EmptyCardLayout, LoaderSpinner } from "@/common";
-import { setAuthInterceptor } from "@/config/axios.config";
+import { ReactNode } from "react";
+import { Navbar, Sidebar } from "@/components";
+import { Layout } from "@/common";
+import DataProvider from "./DataProvider";
+import Aside from "@/components/Aside/Aside";
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
-export default observer(function RootLayout({ children }: RootLayoutProps) {
-  const store = useStore();
-  const [isLoading, setIsLoading] = useState(true);
-  const session = useSession();
-
-  const {
-    user: { setUser },
-    members: { setMembers },
-    products: { setProducts, setTable },
-    shipments: { setShipments },
-    orders: { setOrders },
-  } = store;
-
-  useEffect(() => {
-    if (session.data) {
-      sessionStorage.setItem(
-        "accessToken",
-        session.data.backendTokens.accessToken
-      );
-
-      setUser({
-        _id: session.data.user._id,
-        name: session.data.user.name,
-        image: session.data.user.image,
-        email: session.data.user.email,
-        tenantName: session.data.user.tenantName,
-      });
-
-      if (sessionStorage.getItem("accessToken")) {
-        setAuthInterceptor(sessionStorage.getItem("accessToken"));
-        Memberservices.getAllMembers().then((res) => {
-          setMembers(res);
-        });
-        ProductServices.getAllProducts().then((res) => {
-          setProducts(res);
-        });
-        ProductServices.getTableFormat().then((res) => {
-          setTable(res);
-        });
-
-        setIsLoading(false);
-      }
-    }
-  }, [session, setUser, setMembers, setProducts, setOrders, setShipments]);
-
-  if (!store) return null;
-
-  const tenantNameExists = session.data?.user?.tenantName;
-  if (isLoading) {
-    return <LoaderSpinner />;
-  }
-
-  if (!tenantNameExists) {
-    return (
-      <div className="h-[100vh] p-10">
-        <EmptyCardLayout>
-          <EmptyCard type="registerok" />
-        </EmptyCardLayout>
-      </div>
-    );
-  }
-
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <div className="flex h-screen overflow-hidden">
-      <Sidebar />
-      <aside className="flex flex-col flex-grow  max-h-[100vh] pb-2 ">
-        <Navbar />
-        <Layout>{children}</Layout>
-      </aside>
-      <Aside />
-    </div>
+    <DataProvider>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
+        <section className="flex flex-col flex-grow  max-h-[100vh] pb-2 ">
+          <Navbar />
+          <Layout>{children}</Layout>
+        </section>
+        <Aside />
+      </div>
+    </DataProvider>
   );
-});
+}

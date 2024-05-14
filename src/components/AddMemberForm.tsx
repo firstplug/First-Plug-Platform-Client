a839acc333e927916b7e1be5b270d387ded28f6d
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-05-14 12:21:54 -0300

    feat (table): implemeneted asid for actions btns at product table

--- src/components/AddMemberForm.tsx
+++ src/components/AddMemberForm.tsx
@@ -4,36 +4,65 @@ import { SearchInput } from "@/common";
 import { useStore } from "@/models/root.store";
 import { observer } from "mobx-react-lite";
 import { TeamMember } from "@/types";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 interface AddMemberFormProps {
+  members: TeamMember[];
   handleSelectedMembers: (member: TeamMember) => void;
 }
 
 export const AddMemberForm = observer(function ({
+  members = [],
   handleSelectedMembers,
 }: AddMemberFormProps) {
   const {
-    members: { members },
+    aside: { type },
   } = useStore();
 
   return (
-    <section>
-      <SearchInput placeholder="Search Member" />
-      <div className="flex flex-col gap-3 mt-3">
-        {members.map((member) => (
-          <div className="flex gap-2 items-center" key={member._id}>
-            <input
-              type="checkbox"
-              onChange={() => handleSelectedMembers(member)}
-            />
-            <div className="flex gap-2">
-              <b className="text-black">
-                {member.firstName} {member.lastName}
-              </b>
-              <span className="text-dark-grey">{member._id}</span>
+    <section className="flex flex-col gap-6">
+      <div>
+        <SearchInput placeholder="Search Member" />
+        <div className="flex flex-col gap-3 mt-3">
+          {members.map((member) => (
+            <div
+              className="flex gap-2 items-center py-2 px-4 border cursor-pointer rounded-md transition-all duration-300 hover:bg-hoverBlue "
+              key={member._id}
+              onClick={() => handleSelectedMembers(member)}
+            >
+              <div className="flex gap-2">
+                <p className="text-black font-bold">
+                  {member.firstName} {member.lastName}
+                </p>
+                <span className="text-dark-grey">{member.email}</span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+      {type === "AssingProduct" && (
+        <div className=" flex-col flex gap-2">
+          <h2>Location</h2>
+          <Select>
+            <SelectTrigger className="">
+              <SelectValue placeholder="Location" />
+            </SelectTrigger>
+            <SelectContent>
+              {["FP warehouse", "Our Office", "Employee"].map((location) => (
+                <SelectItem value={location} key={location}>
+                  {location}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      )}
     </section>
   );
 });
---
+++ src/components/AsideContents/AssignProduct.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useStore } from "@/models";
+import { AddMemberForm } from "../AddMemberForm";
+import { useState } from "react";
+import { TeamMember } from "@/types";
+
+export function AssignProduct() {
+  const {
+    members: { members, selectedMemberEmail },
+  } = useStore();
+  const [member, setMember] = useState<TeamMember>();
+
+  const handleSelectedMembers = (member: TeamMember) => {
+    setMember(member);
+  };
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between">
+        <span>Members</span>
+        <span>({members.length})</span>
+      </div>
+
+      <AddMemberForm
+        handleSelectedMembers={handleSelectedMembers}
+        members={members.filter((memb) => memb.email !== selectedMemberEmail)}
+      />
+    </div>
+  );
+}

81ece248a1802895b488052bdffd27d3c7f23f96
MercedesPoisson <mercedespoisson@yahoo.com>
2024-06-07 16:45:38 -0300

    .

--- src/app/home/my-stock/page.tsx
+++ src/app/home/my-stock/page.tsx
@@ -16,30 +16,3 @@ export default observer(function MyStock() {
     </PageLayout>
   );
 });
-
-// "use client";
-// import { observer } from "mobx-react-lite";
-// import { useStore } from "@/models";
-// import DataStock from "./DataStock";
-// import EmptyStock from "./EmptyStock";
-// import { PageLayout } from "@/common";
-// import { BarLoader } from "@/components/Loader/BarLoader";
-
-// export default observer(function MyStock() {
-//   const {
-//     products: { tableProducts },
-//     fetch: { isFetching },
-//   } = useStore();
-
-//   return (
-//     <PageLayout>
-//       {isFetching ? (
-//         <BarLoader />
-//       ) : tableProducts ? (
-//         <DataStock />
-//       ) : (
-//         <EmptyStock />
-//       )}
-//     </PageLayout>
-//   );
-// });

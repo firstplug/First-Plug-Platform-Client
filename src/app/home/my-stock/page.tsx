7acb2e9430844da139a9da7e4c697bd88c4fabc9
MercedesPoisson <mercedespoisson@yahoo.com>
2024-06-07 16:38:35 -0300

    commented loader

--- src/app/home/my-stock/page.tsx
+++ src/app/home/my-stock/page.tsx
@@ -4,23 +4,42 @@ import { useStore } from "@/models";
 import DataStock from "./DataStock";
 import EmptyStock from "./EmptyStock";
 import { PageLayout } from "@/common";
-import { BarLoader } from "@/components/Loader/BarLoader";
 
 export default observer(function MyStock() {
   const {
     products: { tableProducts },
-    fetch: { isFetching },
   } = useStore();
 
   return (
     <PageLayout>
-      {isFetching ? (
-        <BarLoader />
-      ) : tableProducts ? (
-        <DataStock />
-      ) : (
-        <EmptyStock />
-      )}
+      {tableProducts.length === 0 ? <EmptyStock /> : <DataStock />}
     </PageLayout>
   );
 });
+
+// "use client";
+// import { observer } from "mobx-react-lite";
+// import { useStore } from "@/models";
+// import DataStock from "./DataStock";
+// import EmptyStock from "./EmptyStock";
+// import { PageLayout } from "@/common";
+// import { BarLoader } from "@/components/Loader/BarLoader";
+
+// export default observer(function MyStock() {
+//   const {
+//     products: { tableProducts },
+//     fetch: { isFetching },
+//   } = useStore();
+
+//   return (
+//     <PageLayout>
+//       {isFetching ? (
+//         <BarLoader />
+//       ) : tableProducts ? (
+//         <DataStock />
+//       ) : (
+//         <EmptyStock />
+//       )}
+//     </PageLayout>
+//   );
+// });

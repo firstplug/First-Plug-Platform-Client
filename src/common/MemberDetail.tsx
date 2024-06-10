c95bcb1962bedbad13935a42331a37a26f21bc5d
MercedesPoisson <mercedespoisson@yahoo.com>
2024-06-10 18:09:49 -0300

    On Click Fullname open aside with member details

--- src/common/MemberDetail.tsx
+++ src/common/MemberDetail.tsx
@@ -16,60 +16,51 @@ export function MemberDetail({ className }: MemberDetailProps) {
     members: { selectedMember },
   } = useStore();
 
-  return (
-    <div className={`flex gap-2 ${className || ""}`}>
-      <div className="h-[150px] relative aspect-square">
-        <Image
-          src={Photo}
-          alt="member"
-          fill
-          className=" object-cover rounded-md"
-        />
-      </div>
-      <div className="flex flex-col w-full  justify-start  ">
-        <div className="flex w-full justify-between items-center">
-          <div className="flex items-center gap-1">
-            <TeamCard team={selectedMember.team} />
-          </div>
-          <div className="flex  text-dark-grey font-semibold">
-            <Button variant="text">
-              <PenIcon
-                className={
-                  "h-[1rem] aspect-square text-dark-grey font-semibold"
-                }
-                strokeWidth={2}
-              />
-            </Button>
-            <Button variant="text">
-              <TrashIcon
-                className={
-                  " h-[1rem] aspect-square text-dark-grey font-semibold"
-                }
-              />
-            </Button>
-          </div>
-        </div>
+  if (!selectedMember) return null;
 
-        <b className="text-xl text-black">
-          {selectedMember.firstName} {selectedMember.lastName}
-        </b>
-        <div className="flex  items-center   gap-2 text-md mt-2">
-          <div className="flex items-center gap-1 ">
-            <span className="font-semibold "> Date Of Birth: </span>
+  return (
+    <div className={`flex flex-col gap-4 ${className || ""}`}>
+      <div className="flex gap-4">
+        <div className="h-[150px] relative aspect-square">
+          <Image
+            src={selectedMember.picture || Photo}
+            alt="member"
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="flex flex-col w-full justify-start gap-2">
+          <div className="flex w-full justify-between items-center">
+            <div className="flex items-center gap-1">
+              <TeamCard team={selectedMember.team} />
+            </div>
+            <div className="flex text-dark-grey font-semibold gap-2">
+              <Button variant="text">
+                <PenIcon
+                  className="h-[1rem] aspect-square text-dark-grey font-semibold"
+                  strokeWidth={2}
+                />
+              </Button>
+              <Button variant="text">
+                <TrashIcon className="h-[1rem] aspect-square text-dark-grey font-semibold" />
+              </Button>
+            </div>
+          </div>
+          <b className="text-xl text-black">
+            {selectedMember.firstName} {selectedMember.lastName}
+          </b>
+          <div className="flex items-center gap-2 text-md">
+            <span className="font-semibold">Date Of Birth: </span>
             <span className="font-normal">
-              {dateTo_DDMMYY(selectedMember.birthDate)}{" "}
+              {dateTo_DDMMYY(selectedMember.birthDate)}
             </span>
           </div>
-          <span className="text-dark-grey/50">|</span>
-          <div className="flex items-center gap-1 ">
-            <span className="font-semibold ">Joining Date:</span>
+          <div className="flex items-center gap-2 text-md">
+            <span className="font-semibold">Joining Date: </span>
             <span className="font-normal">
               {dateTo_DDMMYY(selectedMember.startDate)}
             </span>
           </div>
-        </div>
-        <div className="flex justify-between">
-          <div className="flex gap-1"></div>
         </div>
       </div>
     </div>

15ee3162936d4ead92198c122b71c1378c0951ae
MercedesPoisson <mercedespoisson@yahoo.com>
2024-06-12 19:52:21 -0300

    search input => filter by name or lastname o email

--- src/common/SearchInput.tsx
+++ src/common/SearchInput.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { SearchIcon } from "./Icons";
-
 
 type SearchInputProps = {
   placeholder?: string;
   className?: string;
   width?: string | number;
   height?: string | number;
-}
+  onSearch?: (value: string) => void;
+};
 
-export function SearchInput({ placeholder, className } : SearchInputProps) {
+export function SearchInput({
+  placeholder,
+  className,
+  onSearch,
+}: SearchInputProps) {
+  const [query, setQuery] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    onSearch && onSearch(event.target.value);
+  };
   return (
     <div
-      className={`flex  border border-grey gap-2 items-center py-2 px-3  rounded-lg  ${className || ""}`}
+      className={`flex  border border-grey gap-2 items-center py-2 px-3  rounded-lg  ${
+        className || ""
+      }`}
     >
       <SearchIcon className={" text-grey font-bold  "} />
       <input
+        type="text"
+        value={query}
+        onChange={handleChange}
         placeholder={placeholder}
         className={`text-md font-normal leading-5  text-dark-grey border-none outline-none  `}
       />
--- src/components/AddMemberForm.tsx
+++ src/components/AddMemberForm.tsx
@@ -16,38 +16,49 @@ export const AddMemberForm = observer(function ({
   selectedMember,
   handleSelectedMembers,
 }: AddMemberFormProps) {
+  const [filteredMembers, setFilteredMembers] =
+    React.useState<TeamMember[]>(members);
+  const handleSearch = (query: string) => {
+    setFilteredMembers(
+      members.filter(
+        (member) =>
+          member.firstName.toLowerCase().includes(query.toLowerCase()) ||
+          member.lastName.toLowerCase().includes(query.toLowerCase()) ||
+          member.email.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+  };
+
   return (
     <section className="flex flex-col gap-6">
-      <div>
-        <SearchInput placeholder="Search Member" />
-        <div className="flex flex-col gap-3 mt-3">
+      <SearchInput placeholder="Search Member" onSearch={handleSearch} />
+      <div className="flex flex-col gap-3 mt-3">
+        <div
+          className={`flex gap-2 items-center py-2 px-4 border cursor-pointer rounded-md transition-all duration-300 hover:bg-hoverBlue ${
+            selectedMember === null ? "bg-hoverBlue" : ""
+          }`}
+          onClick={() => handleSelectedMembers(null)}
+        >
+          <div className="flex gap-2">
+            <p className="text-black font-bold">None</p>
+          </div>
+        </div>
+        {filteredMembers.map((member) => (
           <div
             className={`flex gap-2 items-center py-2 px-4 border cursor-pointer rounded-md transition-all duration-300 hover:bg-hoverBlue ${
-              selectedMember === null ? "bg-hoverBlue" : ""
+              member.email === selectedMember?.email ? "bg-hoverBlue" : ""
             }`}
-            onClick={() => handleSelectedMembers(null)}
+            key={member._id}
+            onClick={() => handleSelectedMembers(member)}
           >
             <div className="flex gap-2">
-              <p className="text-black font-bold">None</p>
+              <p className="text-black font-bold">
+                {member.firstName} {member.lastName}
+              </p>
+              <span className="text-dark-grey">{member.team}</span>
             </div>
           </div>
-          {members.map((member) => (
-            <div
-              className={`flex gap-2 items-center py-2 px-4 border cursor-pointer rounded-md transition-all duration-300 hover:bg-hoverBlue ${
-                member.email === selectedMember?.email ? "bg-hoverBlue" : ""
-              } `}
-              key={member._id}
-              onClick={() => handleSelectedMembers(member)}
-            >
-              <div className="flex gap-2">
-                <p className="text-black font-bold">
-                  {member.firstName} {member.lastName}
-                </p>
-                <span className="text-dark-grey">{member.team}</span>
-              </div>
-            </div>
-          ))}
-        </div>
+        ))}
       </div>
     </section>
   );

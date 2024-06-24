74be7fa9fdd0a6eb682b940f609666ce2212f431
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-06-24 11:44:07 -0300

    fix date format for input default value

--- src/components/AddProduct/InputProductForm.tsx
+++ src/components/AddProduct/InputProductForm.tsx
@@ -31,14 +31,21 @@ export function InputProductForm({
 
   const today = new Date().toISOString().split("T")[0];
 
+  const formattedToday = (date: string) => {
+    const hoy = new Date(date);
+    const yyyy = hoy.getFullYear();
+    const mm = String(hoy.getMonth() + 1).padStart(2, "0"); // Meses empiezan desde 0
+    const dd = String(hoy.getDate()).padStart(2, "0");
+    return `${yyyy}-${mm}-${dd}`;
+  };
+
   return (
     <div className={`relative ${className}`}>
       <label className="block text-dark-grey ml-2 font-sans">{title}</label>
-
       <input
         name={name}
         type={type}
-        value={value}
+        value={type === "date" ? formattedToday(value) : value}
         onChange={(e) => {
           onChange(e);
           if (type === "date") {
